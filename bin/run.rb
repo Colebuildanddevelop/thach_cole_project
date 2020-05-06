5d7dbd05ef6766fef8ecf93e1746868f40990c86
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-05-06 10:51:56 -0500

    created auth class and removed login and create_account methods from run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -8,40 +8,41 @@ require 'pry'
 require_relative '../config/environment'
 $prompt = TTY::Prompt.new
 
-def login
-  #take username and password
-  username = $prompt.ask("Please enter your username.")
-  password = $prompt.mask("Please enter your password.")
-  while !User.find_by(username: username, password: password)
-    puts "oops either the username you entered was not found or the password was incorrect"
-    username = $prompt.ask("Please enter your username.")
-    password = $prompt.mask("Please enter your password.")
-  end
-  User.find_by(username: username, password: password)
-end
 
-def create_account
-  # ask user for username make sure it username doesn't exist
-  username = $prompt.ask("Please create a username.")
-  while User.find_by(username: username)
-    puts "this username is already taken, please choose another"
-    username = $prompt.ask("Please create a username.")
-  end  
-  password = $prompt.mask("Please create a password.")
-  name = $prompt.ask("What is your name?")
-  gender = $prompt.ask("What is your gender?")
-  age = $prompt.ask("What is your age?")
-  desired_workout = $prompt.select("What is your desired style of workout?", %w(high_intensity low_intensity mild_intensity))
+# def login
+#   #take username and password
+#   username = $prompt.ask("Please enter your username.")
+#   password = $prompt.mask("Please enter your password.")
+#   while !User.find_by(username: username, password: password)
+#     puts "oops either the username you entered was not found or the password was incorrect"
+#     username = $prompt.ask("Please enter your username.")
+#     password = $prompt.mask("Please enter your password.")
+#   end
+#   User.find_by(username: username, password: password)
+# end
+
+# def create_account
+#   # ask user for username make sure it username doesn't exist
+#   username = $prompt.ask("Please create a username.")
+#   while User.find_by(username: username)
+#     puts "this username is already taken, please choose another"
+#     username = $prompt.ask("Please create a username.")
+#   end  
+#   password = $prompt.mask("Please create a password.")
+#   name = $prompt.ask("What is your name?")
+#   gender = $prompt.ask("What is your gender?")
+#   age = $prompt.ask("What is your age?")
+#   desired_workout = $prompt.select("What is your desired style of workout?", %w(high_intensity low_intensity mild_intensity))
   
-  user = User.create(
-    username: username,
-    password: password,
-    name: name,
-    gender: gender,
-    age: age,
-    desired_workout: desired_workout
-  )  
-end
+#   user = User.create(
+#     username: username,
+#     password: password,
+#     name: name,
+#     gender: gender,
+#     age: age,
+#     desired_workout: desired_workout
+#   )  
+# end
 
 def user_menu(user)
   session = true
@@ -82,14 +83,16 @@ def user_menu(user)
   end 
 end
 
+
 def main 
   user = nil 
+  auth = Authentication.new
   print_title 
   option = $prompt.select("Please login or create an account", %w(login create_account))
   if option == "create_account"
-    user = create_account
+    user = auth.create_account
   else 
-    user = login
+    user = auth.login
   end 
   user_menu(user)
 end 
